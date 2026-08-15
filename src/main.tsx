d179cb81aed1e927fd3916a49cc81e49dd775070
John <jrahme10@gmail.com>
2026-08-15 21:08:24 +0300

    Mount theme control inside Settings only

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import ThemeControl, { initTheme } from './components/ThemeControl'
+import { initTheme } from './components/ThemeControl'
 import './styles.css'
 import './transaction-polish.css'
 import './exchange.css'
 import './backup.css'
 import './spenza-design.css'
 import './theme-settings.css'
+import './spenza-layout.css'
 import { registerPwa } from './lib/pwa'
 
 initTheme()
@@ -15,7 +16,6 @@ initTheme()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <ThemeControl />
   </React.StrictMode>,
 )
 
