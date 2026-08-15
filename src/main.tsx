7477882265d4983f7275654ae63b0558b81fb9ef
John <jrahme10@gmail.com>
2026-08-15 20:41:20 +0300

    Wire backup import and export into Spenza

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import BackupManager from './components/BackupManager'
 import './styles.css'
 import './transaction-polish.css'
 import './exchange.css'
+import './backup.css'
 import { registerPwa } from './lib/pwa'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <BackupManager />
   </React.StrictMode>,
 )
 
