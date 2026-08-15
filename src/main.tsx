bf54b538e6ada303c06d473afb13d49230b6341e
John <jrahme10@gmail.com>
2026-08-15 20:53:34 +0300

    Render backup only inside Settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import BackupManager from './components/BackupManager'
 import './styles.css'
 import './transaction-polish.css'
 import './exchange.css'
@@ -11,7 +10,6 @@ import { registerPwa } from './lib/pwa'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <BackupManager />
   </React.StrictMode>,
 )
 
