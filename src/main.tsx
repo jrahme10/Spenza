4f46ceb43bdc1ca324876178732285cd719006de
John <jrahme10@gmail.com>
2026-08-15 17:54:09 +0300

    Load polished transaction field styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
+import './transaction-polish.css'
 import { registerPwa } from './lib/pwa'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
