b18c07d19a1633c28c6f2162a197dd88ef79d74d
John <jrahme10@gmail.com>
2026-08-15 20:59:38 +0300

    Enable Spenza dark and light redesign

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ThemeControl, { initTheme } from './components/ThemeControl'
 import './styles.css'
 import './transaction-polish.css'
 import './exchange.css'
 import './backup.css'
+import './spenza-design.css'
 import { registerPwa } from './lib/pwa'
+
+initTheme()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ThemeControl />
   </React.StrictMode>,
 )
 
