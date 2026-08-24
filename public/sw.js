749b8fb063286c3e7e90dbb9f0ce4911a0304e99
John <jrahme10@gmail.com>
2026-08-24 22:21:09 +0300

    Bump PWA cache after category picker fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='spenza-v42'
+const CACHE_NAME='spenza-v43'
 const APP_SHELL=['./','./manifest.webmanifest','./icon.svg']
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)))})
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))})
