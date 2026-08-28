f04f07f58049a96b7d68c19bc3ed0e813099e79d
John <jrahme10@gmail.com>
2026-08-28 23:06:09 +0300

    Wire native app data provider

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { AppDataProvider } from '@/lib/AppDataContext'
 import { colors } from '@/lib/theme'
 
 export default function RootLayout(){
-  return <>
+  return <AppDataProvider>
     <StatusBar style="light"/>
     <Stack screenOptions={{headerShown:false,contentStyle:{backgroundColor:colors.background}}}/>
-  </>
+  </AppDataProvider>
 }
