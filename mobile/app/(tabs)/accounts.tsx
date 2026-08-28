a0ccf9ba90616b5fb97fd5d0f3b5c7aa18800a51
John <jrahme10@gmail.com>
2026-08-28 21:18:38 +0300

    Add native Accounts screen

---
+++ mobile/app/(tabs)/accounts.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native'
+import { Screen } from '@/components/Screen'
+import { colors } from '@/lib/theme'
+
+export default function AccountsScreen(){return <Screen title="Accounts"><Text style={{color:colors.muted}}>Wallet cards and default account will be migrated here.</Text></Screen>}
